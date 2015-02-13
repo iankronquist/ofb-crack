6ca2c36e551a1b1193324e0c8273313652bb9c63
Ian Kronquist <iankronquist@teleport.com>
2015-02-13 11:06:51 -0800

    Fix syntax errors with oracle server

--- oracle_server.py
+++ oracle_server.py
@@ -1,11 +1,10 @@
-from operator import operator+
-from os import environ
+from operator import add
+from os import environ, urandom
 from flask import Flask, request, render_template, flash, redirect, url_for, g
 import ofbmode
 
-key = environ['CRYPTO_KEY']
+key = environ.get('CRYPTO_KEY') or urandom(ofbmode.BLOCK_SIZE)
 app = Flask(__name__)
-app.secret_key = environ['APP_KEY']
 
 @app.route('/')
 def about():
@@ -14,18 +13,22 @@ def about():
 @app.route('/null', methods=['POST'])
 def null_byte_oracle():
     ciphertext = request.form['ciphertext']
-    plaintext = reduce(operator+, ofbmode.ofb_crypt_str(ciphertext, key))
+    plaintext = reduce(add, ofbmode.ofb_crypt_str(ciphertext, key))
     if '\0' in plaintext:
         return 400
     else:
         return 200
 
 @app.route('/padding', methods=['POST'])
-def null_byte_oracle():
+def padding_oracle():
     ciphertext = request.form['ciphertext']
-    plaintext = reduce(operator+, ofbmode.ofb_crypt_str(ciphertext, key))
+    plaintext = reduce(add, ofbmode.ofb_crypt_str(ciphertext, key))
     padding_len = ord(plaintext[-1])
-    if any(map(lambda a: a != '\0', plaintext[-padding_len:-1]:
+    if any(map(lambda a: a != '\0', plaintext[-padding_len:-1])):
         return 400
     else:
         return 200
+
+if __name__ == '__main__':
+    print 'app running'
+    app.run(debug=True)
