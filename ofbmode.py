6863601700b3d971faf0c0cdf979de58a947a3f2
Ian Kronquist <iankronquist@teleport.com>
2015-02-13 15:51:39 -0800

    Add tests for the OFB mode suite

--- ofbmode.py
+++ ofbmode.py
@@ -1,72 +1,87 @@
 import os
+import random
+from operator import xor
 
-BLOCK_SIZE = 1024
+
+BLOCK_SIZE = 256
+KEY_SIZE = 1024
+ 
+def hex_print(ba):
+    """Prints the colon separated hexidecimal values of each byte in a
+    bytearray"""
+    print ":".join("{:02x}".format(c) for c in ba)
+
+
+def PRF(text, key):
+    # FIXME: This is *definitely* a secure PRF
+    random.seed(str(text) + str(key))
+    return bytearray([random.randint(0, 255) for _ in xrange(len(text))])
+
 
 def ofb_block(inbound, text, key):
+    """A single OFB mode sub-section.
+    bytearray -> bytearray -> bytearray -> bytearray"""
     stream = block_cipher(inbound, key)
     outbound = XOR(text, stream)
     return outbound
 
-def XOR(a, b):
-    """xors two strings. Assumes that both strings are of the same length"""
-    return map(lambda a_byte, b_byte: chr(ord(a_byte) ^ ord(b_byte)), a, b)
-
-def PRF(text, key):
-    :
-
+       
 def block_cipher(text, key):
+    """A four round block cipher, straight out of the book"""
     # Assert that the text is of an even length
     assert len(text) & 1 == 0
     half = len(text)/2
     left = text[:half]
     right = text[half:]
-    for _ in range(4):
+    for block_round in range(1):
         tmp = right
         right = PRF(XOR(left, right), key)
         left = tmp
     return left + right
     
 
-def ofb_crypt_file(filename, key):
-    """
-    A generator which yields the plaintext from an encrypted or decrypted
-    file using OFB and
-    """
-    with open(filename, 'r') as f:
-        # Initialize prev_block with IV
-        prev_block = os.urandom(BLOCK_SIZE)
-        while True:
-            # Read a block worth of text from the file
-            block = f.read(BLOCK_SIZE)
-            # If the block is too short, pad it according to my understanding
-            # ANSI X.92
-            if len(block) < BLOCK_SIZE
-                padding_zeros = BLOCK_SIZE - len(block) - 1
-                block += '\0' * padding_zeros
-                block += chr(padding_zeros)
-                yield ofb_block(prev_block, block)
-                break
-            yield ofb_block(prev_block, block)
-
-def ofb_crypt_str(string, key):
-    """
-    A generator which yields the plaintext from an encrypted or decrypted
-    string using OFB and
-    """
+def ofb_encrypt(plain, key):
+    plain = pad(plain)
     # Initialize prev_block with IV
-    prev_block = os.urandom(BLOCK_SIZE)
-    for block in chunks(string, BLOCK_SIZE):
-        # If the block is too short, pad it according to my understanding
-        # ANSI X.92
-        if len(block) < BLOCK_SIZE
-            padding_zeros = BLOCK_SIZE - len(block) - 1
-            block += '\0' * padding_zeros
-            block += chr(padding_zeros)
-            yield ofb_block(prev_block, block)
-            break
-        yield ofb_block(prev_block, block)
+    prev_block = bytearray(os.urandom(BLOCK_SIZE))
+    yield prev_block
+    for block in blocks(plain, BLOCK_SIZE):
+        yield ofb_block(prev_block, block, key)
+        prev_block = block
 
-def chunks(string, block_len):
-    """ Yield successive n-sized chunks from string."""
-    for i in xrange(0, len(string), block_len):
-        yield string[i:i+block_len]
+
+def ofb_decrypt(cipher, key):
+    gen_blocks = blocks(cipher, BLOCK_SIZE)
+    # The first block is the IV, don't decrypt it.
+    prev_block = next(gen_blocks)
+    for block in gen_blocks:
+        yield ofb_block(prev_block, block, key)
+        prev_block = block
+        
+
+def pad(block):
+    pad_zeros = (BLOCK_SIZE - len(block)) - 1
+    if pad_zeros == 0:
+        pad_zeros = BLOCK_SIZE - 1
+    block += '\0' * pad_zeros
+    block += chr(pad_zeros)
+    assert len(block) % BLOCK_SIZE == 0
+    return block
+
+
+def strip(block):
+    padding = block[-1]
+    # Don't check the padding - that can lead to a padding oracle attack
+    # assert all(map(lambda x: x == 0, block[len(block)-padding:-1]))
+    return block[:(len(block) - padding - 1)]
+
+
+def XOR(a, b):
+    """xors two bytearrays of equal length"""
+    return bytearray(map(xor, a, b))
+
+
+def blocks(ba, block_len):
+    """Yield successive n-sized chunks from the bytearray."""
+    for i in xrange(0, len(ba), block_len):
+        yield ba[i:i+block_len]
--- test_ofbmode.py
+++ test_ofbmode.py
@@ -1,13 +1,38 @@
+import os
 import unittest
-import mock
+import operator
 import ofbmode
 
 
 class TestOfbMode(unittest.TestCase):
+
     def test_xor(self):
-        a = 'abcd'
-        zeros = '\0' * 4
-        ones = '\xff' * 4
+        a = bytearray('abcd')
+        zeros = bytearray('\0' * 4)
+        ones = bytearray('\xff' * 4)
         self.assertEqual(ofbmode.XOR(a, a), zeros)
         self.assertEqual(ofbmode.XOR(a, zeros), a)
-        #self.assertEqual(XOR(a, ones), a_inverse)
+
+    def test_strip(self):
+        s = bytearray('test\x00\x00\x00\x03')
+        self.assertEqual(bytearray('test'), ofbmode.strip(s))
+
+    def test_pad(self):
+        t = bytearray('test')
+        expected = bytearray('test' + '\x00' * 251 + chr(251))
+        self.assertEqual(expected, ofbmode.pad(t))
+    
+    def test_ofb_crypt(self):
+        key = bytearray(os.urandom(ofbmode.KEY_SIZE))
+        plaintext = bytearray('hello world')
+        ciphertext = ofbmode.ofb_encrypt(plaintext, key)
+        ciphertext_str = reduce(operator.add, ciphertext)
+        dec_plaintext = ofbmode.ofb_decrypt(ciphertext_str, key)
+        dec_plaintext_str = reduce(operator.add, dec_plaintext)
+        self.assertNotEqual(ciphertext_str, dec_plaintext_str)
+        self.assertEqual(plaintext, dec_plaintext_str)
+
+    def test_blocks(self):
+        a = bytearray('12345')
+        self.assertEqual(map(lambda x: x, ofbmode.blocks(a, 2)),
+            map(bytearray, ['12', '34', '5']))
