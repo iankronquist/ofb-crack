03f48a179bd1b7d8c12129da99a4340d0c39001d
Ian Kronquist <iankronquist@teleport.com>
2015-02-13 17:59:06 -0800

    Update according to change ofbmode api

--- oracle_server.py
+++ oracle_server.py
@@ -13,7 +13,7 @@ def about():
 @app.route('/null', methods=['POST'])
 def null_byte_oracle():
     ciphertext = request.form['ciphertext']
-    plaintext = reduce(add, ofbmode.ofb_crypt_str(ciphertext, key))
+    plaintext = reduce(add, ofbmode.ofb_decrypt(ciphertext, key))
     if '\0' in plaintext:
         return 400
     else:
@@ -22,7 +22,7 @@ def null_byte_oracle():
 @app.route('/padding', methods=['POST'])
 def padding_oracle():
     ciphertext = request.form['ciphertext']
-    plaintext = reduce(add, ofbmode.ofb_crypt_str(ciphertext, key))
+    plaintext = reduce(add, ofbmode.ofb_decrypt(ciphertext, key))
     padding_len = ord(plaintext[-1])
     if any(map(lambda a: a != '\0', plaintext[-padding_len:-1])):
         return 400
